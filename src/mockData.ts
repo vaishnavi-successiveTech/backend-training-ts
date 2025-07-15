5eafbb3e6ab9b991151102a24d217910fbe72e6e
Vaishnavi Gupta <vaishnavi.gupta@vaishnavivrs-18.successivetech.com>
2025-07-15 18:48:27 +0530

    few changes

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,6 +1,8 @@
-export const users = [
-  { id: 1, name: 'Vaishnavi', email: 'vaish@example.com' },
-  { id: 2, name: 'Aryan', email: 'aryan@example.com' },
-  { id: 3, name: 'Priya', email: 'priya@example.com' },
-  { id: 4, name: 'Rohan', email: 'rohan@example.com' }
+import { User } from "./types";
+
+export const users:User[] = [
+  { id: 1, name: 'Vaishnavi', email: 'vaish@example.com' ,role:"Admin"},
+  { id: 2, name: 'Aryan', email: 'aryan@example.com' ,role:"User"},
+  { id: 3, name: 'Priya', email: 'priya@example.com',role:"User" },
+  { id: 4, name: 'Rohan', email: 'rohan@example.com' , role:"User"}
 ];
