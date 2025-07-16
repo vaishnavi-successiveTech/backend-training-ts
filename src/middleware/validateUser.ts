efd48df777a49ec7f71104b8fc20560fc4ada6dd
Vaishnavi Gupta <vaishnavi.gupta@vaishnavivrs-18.successivetech.com>
2025-07-16 16:00:35 +0530

    fix:Error problem

--- src/middleware/validateUser.ts
+++ src/middleware/validateUser.ts
@@ -5,12 +5,23 @@ export const validateUsers= (req:Request,res:Response,next:NextFunction)=>{
     const {name,password} = req.body;
     let isAuthorized=false;
     console.log("name is",name);
+    console.log(password)
     if(name && password){
         isAuthorized=true;
         console.log("Login successfully")
         next();
-    }else{
-        console.log("pending")
+    }
+    else if(name && !password){
+        console.log("Name is given but not password");
+        res.status(501).send("Error occured password not present");
+
+    }
+    else if(!name && password){
+        console.log("Password is given but not name");
+        res.status(501).send("Error occured name not present");
+    }
+    else{
+        console.log("some error")
     }
 }
 export const validateUserJwt=(req:Request,res:Response,next:NextFunction)=>{
