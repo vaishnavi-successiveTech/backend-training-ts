3ad992ab74db78f10e9b9e07d4749178f1a6c6c6
Vaishnavi Gupta <vaishnavi.gupta@vaishnavivrs-18.successivetech.com>
2025-08-01 17:24:00 +0530

    fix:app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,8 @@
-// import express, { Request, Response } from 'express';
-// import { users } from './mockData';
-
-// const app = express();
-// const PORT = 3000;
-
-// app.get('/api/users', (req: Request, res: Response) => {
-//   res.json(users);
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Express API running at http://localhost:${PORT}/api/users`);
-// });
-
+import { startServer } from './server';
 import express from 'express';
-import dotenv from 'dotenv';
-import { router } from "./routes/userRouter";
-import { errorHandler } from './middleware/errorHandle';
 
-import { CustomHeader } from './middleware/CustomHeader';
-
-import { ErrorMiddleWare } from './middleware/errorMiddleware';
-import { connnection } from './config/db';
-import { countryRoute } from './routes/CountryRoutes';
-import seedCountries from './modules/country/seedCountries';
-import { basicLimiter } from './middleware/ratelimiter';
-
-
-
-dotenv.config();
-
-const errorHandle=new ErrorMiddleWare();
 const app = express();
-const PORT = 3000;
-const limit=5;
-
-const custom=new CustomHeader(); //
-app.use(basicLimiter(limit, 60000));
 app.use(express.json());
-app.use(custom.customHeader("created-by",'vaishnavi'))
-// app.use(logger); // custom middleware for timestamp
-app.use("/api", router);
-// app.use("/error",dynamicError);
-app.use("/country",countryRoute);
-app.use(errorHandle.errorHandleMiddleware);
+startServer();
 
-app.use(errorHandler); // Called for all errors, including 404, 422, 500
-// app.listen(PORT, () => {
-//   console.log(` Server is running on port ${PORT}`);
-// });
-// connnection().then(()=>{
-//   app.listen(3000,()=>{
-//     console.log("app is running on port 3000");
-//   })
-// })
-connnection()
-  .then(async () => {
-    await seedCountries();
-    app.listen(PORT, () => {
-      console.log( `Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Failed to connect to DB:', err);
-  });
+
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,44 @@
-import mongoose  from "mongoose";
 
-export const connnection=async()=>{
+import mongoose from "mongoose";
+import CountryModel from "../models/countryModel";
+import { countriesDetails } from "../utils/mockData";
+export class ConnectionDb {
 
-    try{
-        const mongo_url=process.env.MONGO_URI!;
-        await mongoose.connect(mongo_url);
-        console.log("connncted to db typescript ");
+  public async connectDb(): Promise<void> {
+    try {
+      await mongoose.connect(process.env.MONGO_URI!);
+      console.log("Connected to MongoDB.");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:",error);
+      process.exit(1);
     }
-    catch(error){
-        console.log("Cannot connect to databse")
-    }
+  }
+public async seedCountries(): Promise<void> {
+  try {
+    await this.connectDb();
+    await CountryModel.deleteMany({});
+    console.log("Existing countries deleted.");
+    await CountryModel.insertMany(countriesDetails);
+    console.log("Countries seeded successfully.");
+  } catch (error) {
+    console.error("Seeding failed",error);
+  } finally {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDb.");
+  }
 }
+}
+
+// import mongoose  from "mongoose";
+
+// export const connnection=async()=>{
+
+//     try{
+//         const mongo_url=process.env.MONGO_URI!;
+//         await mongoose.connect(mongo_url);
+//         console.log("connncted to db typescript ");
+//     }
+//     catch(error){
+//         console.log("Cannot connect to databse")
+//     }
+// }
