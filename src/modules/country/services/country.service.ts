e6901a2ccf87e0549915dc5c483c50c1c1c6c460
Vaishnavi Gupta <vaishnavi.gupta@vaishnavivrs-18.successivetech.com>
2025-08-06 14:16:51 +0530

    fix:Conflicts

--- src/modules/country/services/country.service.ts
+++ src/modules/country/services/country.service.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-import { ICountry } from "../../../entities/ICountry";
-=======
->>>>>>> 3ad992ab74db78f10e9b9e07d4749178f1a6c6c6
+
 import { countryRepository } from "../repository/country.repo";
 // import { countriesDetails } from "../seedCountries";
 
